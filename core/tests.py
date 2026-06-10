a3ad2de852e208bc5c98c08b3a2901aef2ca2dcb
Wiktor Szymański <25967@edu.amw.gdynia.pl>
2026-06-10 04:27:22 +0200

    Fix remaining flake8 spacing issues

--- core/tests.py
+++ core/tests.py
@@ -1,9 +1,11 @@
 from django.test import TestCase
 
+
 class IndexViewTest(TestCase):
     def test_index_status_code(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+
     def test_index_json_content(self):
         response = self.client.get('/')
         self.assertEqual(response.json(), {'message': 'Hello CI/CD World!'})
