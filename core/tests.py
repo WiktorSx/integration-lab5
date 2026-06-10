4a5c5cc015d30dd07959bff49f4d58933439c3a0
Wiktor Szymański <25967@edu.amw.gdynia.pl>
2026-06-10 04:24:55 +0200

    Fix flake8 linter formatting issues

--- core/tests.py
+++ core/tests.py
@@ -1,12 +1,9 @@
 from django.test import TestCase
 
 class IndexViewTest(TestCase):
-    # Test 1: Sprawdza czy endpoint zwraca status 200
     def test_index_status_code(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-
-    # Test 2: Sprawdza czy endpoint zwraca poprawna wiadomosc
     def test_index_json_content(self):
         response = self.client.get('/')
         self.assertEqual(response.json(), {'message': 'Hello CI/CD World!'})
